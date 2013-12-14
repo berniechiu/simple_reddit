442a349bcf434aa77dc289de7020e04fae6f549c
Bernie Chiu <bernie_chiu@hotmail.com>
2013-12-14 20:34:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Simple Reddit
+
+This is a simple Reddit project followed along with the instructions by Schneems [Reddit on Rails Project](https://github.com/schneems/reddit_on_rails)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleReddit::Application.config.secret_token = '1cbf4e5e45c5c841bf6038aeb56a79bd7bb898447a47f99505749a933c4232bf981272485fd33c2c9f53f9641304d876c5b15e046ec31c4e7c13786562137eb0'
