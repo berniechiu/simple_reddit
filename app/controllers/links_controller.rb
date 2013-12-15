db4e4150cd245b765d00cd02902d5ec34c0b152d
Bernie Chiu <bernie_chiu@hotmail.com>
2013-12-15 13:30:43 +0800

    Build relation and views

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,5 +7,12 @@ class LinksController < ApplicationController
   end
 
   def create
+    @link = current_user.links.build(params[:link])
+
+    if @link.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
   attr_accessible :title, :url
   belongs_to :user
+  validates :title, presence: true
+  validates :url,   presence: true
 end
