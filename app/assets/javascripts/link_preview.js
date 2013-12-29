a57559ac601ab58d8b6b5463e46ab1d56069cc7c
Bernie Chiu <bernie_chiu@hotmail.com>
2013-12-29 18:21:36 +0800

    Link preview feature

---
+++ app/assets/javascripts/link_preview.js
@@ -0,0 +1,8 @@
+// Set up preview.
+$('#link_url').preview({key:'9ec348250791474292517180061d12ce'})
+  .on('loading', function(){
+    $(this).prop('disabled', true);
+  }).on('loaded', function(){
+    $(this).prop('disabled', false);
+    $('input#link-submit').val('Share Link');
+  });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def load_page_specific_javascripts(*files)
+    content_for(:page_specific_scripts) { javascript_include_tag(*files) }
+  end
 end
