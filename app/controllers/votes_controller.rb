b33614b31a50c8500d41946c002d11d7f914e572
Bernie Chiu <bernie_chiu@hotmail.com>
2014-01-04 21:38:41 +0800

    Refactor codes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,13 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!, only: [:create]
 
   def create
-    if current_user.votes.where(link_id: params[:vote][:link_id]).present?
-      vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+    votes = current_user.votes.where(link_id: params[:vote][:link_id])
+
+    if votes.present?
+      vote = votes.first
       vote.update_attributes(params[:vote])
     else
-      vote = current_user.votes.create(params[:vote])
-      vote.save!
+      current_user.votes.create(params[:vote])
     end
 
     redirect_to root_path
