782a30fa5456af7f068a128c37821d1d4eceb210
Bernie Chiu <bernie_chiu@hotmail.com>
2014-04-03 07:59:04 +0800

    Add index to category name

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140402082555) do
+ActiveRecord::Schema.define(:version => 20140402235152) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "categories", ["name"], :name => "index_categories_on_name"
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
