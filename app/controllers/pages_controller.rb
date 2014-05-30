27bc6510dab3f9d434af14ee085884a6b0e4ee9e
Bernie Chiu <bernie_chiu@hotmail.com>
2014-05-30 16:11:16 +0800

    Categories filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,33 @@
 class PagesController < ApplicationController
   def index
+    @all_categories = Category.all
+
     @newest_links = Link.includes(:votes)
                         .newest
                         .paginate(:page => params[:page], :per_page => 8)
     @popular_links = Link.includes(:category, :votes).top
 
     @videos = Video.recent
+
+    categories_filter
   end
+
+  private
+
+    def categories_filter
+      session[:categories] = params[:categories]
+
+      if session[:categories]
+
+        @categories = session[:categories]
+        @categories = @categories.values if @categories.respond_to?(:values)
+        @links = Link.includes(:category)
+                     .find(:all,
+                           conditions: ["category_id IN (?)", @categories])
+      end
+
+      if session[:categories] != params[:categories]
+        redirect_to root_path(categories: session[:categories])
+      end
+    end
 end
