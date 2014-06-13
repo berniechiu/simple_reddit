14a50d3463b259a38437b144af9b1aab03cb65a8
Bernie Chiu <bernie_chiu@hotmail.com>
2014-06-13 12:57:11 +0800

    Update assets config

--- app/assets/javascripts/video_preview.js
+++ app/assets/javascripts/video_preview.js
@@ -1,8 +1,8 @@
 // Set up preview.
-$('#link_url').preview({key: "<%= Setting.embedly_api_key %>"})
+$('#video_link').preview({key: "9ec348250791474292517180061d12ce"})
   .on('loading', function(){
     $(this).prop('disabled', true);
   }).on('loaded', function(){
     $(this).prop('disabled', false);
-    $('input#link-submit').val('Share Link');
+    $('input#link-submit').val('Share Video');
   });
